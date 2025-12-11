fcdd059c9e675562ae064d40415b3f8693e9679d
Tóth Gergely Árpád <h378654@stud.u-szeged.hu>
2025-12-12 00:48:10 +0100

    drop q-val ha hoverelunk az item fellett,dropped itemek megcsinalasa,droppolt itemek felvetele,inventory tabbal mukodese , tabos mukodos szinkronizacioja a chestel

--- Library/PackageCache/com.unity.ai.inference@587873fd5e1b/Runtime/Tokenization/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.InferenceEngine.EditorTests")]
-
-// Required for advanced mocking with Moq
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
